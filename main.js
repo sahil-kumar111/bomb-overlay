3631b5a17dfa6e1a57b5ee8a9a09232dfd8fcb50
sahil-kumar111 <150124199+sahil-kumar111@users.noreply.github.com>
2026-03-01 13:13:55 +0530

    Initial bomb overlay app

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow, screen } = require('electron');
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  const win = new BrowserWindow({
+    width: 350,
+    height: 220,
+    x: 0,                         // LEFT edge
+    y: height - 220,              // BOTTOM edge
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    resizable: false,
+    hasShadow: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('Bomb.html');
+}
+
+app.whenReady().then(createWindow);
